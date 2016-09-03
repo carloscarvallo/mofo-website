8ac45204a54cb003a92cb99f86a2f01eb78f47cb
carloscarvallo <erotomania669@gmail.com>
2016-09-03 16:51:55 -0400

    feat: preparing to serve static files

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const express = require('express')
 , port = process.env.PORT || 8080
 
 app.use(morgan('dev'))
+app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
